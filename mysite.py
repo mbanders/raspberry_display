dbca4ae3cebddb006e8f59915cbb98969a7cd4ad
Mike Anderson <mbanderson@uwalumni.com>
2016-04-23 13:57:23 -0500

    add ifconfig data
    Signed-off-by: Mike Anderson <mbanderson@uwalumni.com>

--- mysite.py
+++ mysite.py
@@ -3,13 +3,28 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Python libraries
+import datetime
+import re
 import subprocess
+
+prev_info = {'time': datetime.datetime.now(), 'rx_mbytes': 0, 'tx_mbytes':0}
 
 # Main app
 @app.route('/')
 def hello_world():
+    global prev_info
     info = {}
-    info['uptime'] = subprocess.check_output(['uptime -s'], shell=True)
+    iface = 'eth0'
+    ifconfig = subprocess.check_output("ifconfig %s" % iface, shell=True)
+    info['time'] = datetime.datetime.now()
+    info['rx_mbytes'] = int(re.search(' RX bytes:(\d+) ', ifconfig).group(1))/125000.
+    info['tx_mbytes'] = int(re.search(' TX bytes:(\d+) ', ifconfig).group(1))/125000.
+    info['rx_rate'] = (info['rx_mbytes'] - prev_info['rx_mbytes']) / (info['time'] - prev_info['time']).total_seconds()
+    info['tx_rate'] = (info['tx_mbytes'] - prev_info['tx_mbytes']) / (info['time'] - prev_info['time']).total_seconds()
+    info['rx_rate'] = "%.2f" % info['rx_rate']
+    info['tx_rate'] = "%.2f" % info['tx_rate']
+    info['uptime'] = subprocess.check_output('uptime -p', shell=True)
+    prev_info = info
     return render_template('index.html', info=info)
 
 if __name__ == '__main__':
